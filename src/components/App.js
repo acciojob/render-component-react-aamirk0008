185947d07629a122446eb7deb3437e204d153f74
Sheikh Aamir <113338901+aamirk0008@users.noreply.github.com>
2025-08-29 19:17:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import para from './index.jsx'
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <div id='root'>
+    <para/>
+    </div>
+    </div>
   )
 }
 
