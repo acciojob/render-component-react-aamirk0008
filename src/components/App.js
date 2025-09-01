c7a49f9daaee331b7e2fca2370e9bb811aa44e12
Sheikh Aamir <113338901+aamirk0008@users.noreply.github.com>
2025-09-01 15:42:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-import para from './index'
 
 const App = () => {
   return (
     <div id="main">
-    <div id="root">
-    <para/>
-    </div>
+    <p>I am learning React. My life is getting better</p>
     </div>
   )
 }
